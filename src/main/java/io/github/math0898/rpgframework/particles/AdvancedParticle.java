18f7657fdd3aaa103e3550fb19a69101e80b2a1c
math0898 <25396616+math0898@users.noreply.github.com>
2023-01-25 16:34:56 -0600

    Added and Implemented AdvancedParticle

---
+++ src/main/java/io/github/math0898/rpgframework/particles/AdvancedParticle.java
@@ -0,0 +1,46 @@
+package io.github.math0898.rpgframework.particles;
+
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AdvancedParticle is a type of particle made from multiple SubParticles.
+ *
+ * @author Sugaku
+ */
+public class AdvancedParticle {
+
+    /**
+     * A list of SubParticles that should be spawned when this particle is.
+     */
+    private final List<SubParticle> particles = new ArrayList<>();
+
+    /**
+     * Creates a new AdvancedParticle without any sub-particles.
+     */
+    public AdvancedParticle () {
+
+    }
+
+    /**
+     * Adds a SubParticle to this AdvancedParticle.
+     *
+     * @param s The SubParticle to add.
+     */
+    public void addSubParticle (SubParticle s) {
+        particles.add(s);
+    }
+
+    /**
+     * Spawns this particle into the world at the given location.
+     *
+     * @param loc The location to spawn this particle in at.
+     */
+    public void spawnParticle (Location loc) {
+        final Location locale = loc;
+        if (locale.getWorld() == null) return;
+        particles.forEach((p) -> p.spawnParticle(locale));
+    }
+}
