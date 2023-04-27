8a63675eb8e8fd990bd3c1bfcbd49416b15bbef8
math0898 <25396616+math0898@users.noreply.github.com>
2023-04-26 19:22:47 -0500

    Defined Krusk YAML

--- src/main/java/io/github/math0898/rpgframework/enemies/bosses/Boss.java
+++ src/main/java/io/github/math0898/rpgframework/enemies/bosses/Boss.java
@@ -1,5 +1,7 @@
 package io.github.math0898.rpgframework.enemies.bosses;
 
+import org.bukkit.configuration.ConfigurationSection;
+
 /**
  * The Boss class which describes generic bosses and their abilities.
  *
@@ -7,4 +9,21 @@ package io.github.math0898.rpgframework.enemies.bosses;
  */
 public class Boss {
 
+    /**
+     *
+     */
+
+    /**
+     * Creates a new Boss prototype using the given configuration file to define mob values.
+     */
+    public Boss (ConfigurationSection config) {
+
+    }
+
+    /**
+     * Spawns this boss at the given location.
+     *
+     * @param locale The location to spawn this boss at.
+     */
+    public
 }
