5419ceaafabc5009e799abb61df3aceaf2f4abaf
math0898 <25396616+math0898@users.noreply.github.com>
2024-03-20 16:52:38 -0500

    Added Feyrith as Summonable

--- src/main/java/io/github/math0898/rpgframework/commands/SummonRPG.java
+++ src/main/java/io/github/math0898/rpgframework/commands/SummonRPG.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import sugaku.rpg.framework.mobs.MobManager;
 import sugaku.rpg.mobs.teir1.eiryeras.EiryerasBoss;
+import sugaku.rpg.mobs.teir1.feyrith.FeyrithBoss;
 import sugaku.rpg.mobs.teir1.krusk.KruskBoss;
 import sugaku.rpg.mobs.teir1.krusk.KruskMinion;
 
@@ -52,6 +53,9 @@ public class SummonRPG extends BetterCommand {
         } else if (args[0].equalsIgnoreCase("KruskMinion")) {
             send(player, "One of " + ChatColor.GREEN +  "Krusk's" + ChatColor.GRAY + " minions has been summoned.");
             for (int i = 0; i < loop; i++) MobManager.addMob(new KruskMinion(player.getLocation(), (int) (new Random().nextDouble() * 8)));
+        } else if (args[0].equalsIgnoreCase("Feyrith")) {
+            send(player, "One of " + ChatColor.BLUE +  "Feyrith" + ChatColor.GRAY + " has been summoned.");
+            MobManager.addMob(new FeyrithBoss(player.getLocation()));
         } else send(player, ChatColor.RED + "Sorry but we could not find that mob.");
         return true;
     }
