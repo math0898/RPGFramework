7545f38cc246d11421494769d07a67523e7f2f12
math0898 <25396616+math0898@users.noreply.github.com>
2025-09-22 18:50:35 -0500

    Attempted to Add ItemName Text. Positioning is off.

--- studio/src/main/java/io/github/math0898/studio/ItemPreview.java
+++ studio/src/main/java/io/github/math0898/studio/ItemPreview.java
@@ -1,9 +1,11 @@
 package io.github.math0898.studio;
 
+import io.github.math0898.utils.Utils;
 import suga.engine.game.objects.BasicGameObject;
 import suga.engine.graphics.GraphicsPanel;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * An ItemPreview is a preview of how an item will appear in game based upon the given values.
@@ -43,6 +45,31 @@ public class ItemPreview extends BasicGameObject {
     private static final int INNER_BORDER_WIDTH = 2;
 
     /**
+     * The horizontal offset for text.
+     */
+    private static final int TEXT_HORIZONTAL_OFFSET = 8;
+
+    /**
+     * The separation between text lines.
+     */
+    private static final int TEXT_VERTICAL_BUFFER = 6;
+
+    /**
+     * The separation between the item name text and the first row of lore/enchantments.
+     */
+    private static final int TITLE_VERTICAL_BUFFER = 8;
+
+    /**
+     * The separation between the item name text and the top of the item box.
+     */
+    private static final int TITLE_VERTICAL_OFFSET = 8;
+
+    /**
+     * The size of the text font used.
+     */
+    private static final int TEXT_FONT_SIZE = 16;
+
+    /**
      * Called every drawing frame so programs have a chance to make their voices heard on what gets drawn.
      *
      * @param width  The width of the pixel map.
@@ -53,8 +80,12 @@ public class ItemPreview extends BasicGameObject {
     public void applyChanges (int width, int height, GraphicsPanel panel) {
         final int posX = 100;
         final int posY = 100;
+
         panel.setRectangle(posX, posY, BACKGROUND_BOX_WIDTH, BACKGROUND_BOX_HEIGHT, BACKGROUND_COLOR);
         panel.setRectangle(posX + INNER_BORDER, posY + INNER_BORDER, BACKGROUND_BOX_WIDTH - (2 * INNER_BORDER), BACKGROUND_BOX_HEIGHT - (2 * INNER_BORDER), HIGHLIGHT_COLOR);
         panel.setRectangle(posX + INNER_BORDER + INNER_BORDER_WIDTH, posY + INNER_BORDER + INNER_BORDER_WIDTH, BACKGROUND_BOX_WIDTH - (2 * (INNER_BORDER_WIDTH + INNER_BORDER)), BACKGROUND_BOX_HEIGHT - (2 * (INNER_BORDER_WIDTH + INNER_BORDER)), BACKGROUND_COLOR);
+
+        BufferedImage itemName = Utils.imageFromText(new Font("Comic Sans", Font.BOLD, 14), new Color(81, 197, 115), "Krusk's Axe");
+        panel.addImage(posX + TEXT_HORIZONTAL_OFFSET, posY + TITLE_VERTICAL_BUFFER - itemName.getHeight(), itemName.getWidth(), itemName.getHeight(), itemName);
     }
 }
