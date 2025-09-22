f5a4b477e9be8752600cc92ca5c18e5194a0063f
math0898 <25396616+math0898@users.noreply.github.com>
2025-09-22 18:36:50 -0500

    Background Box Made

--- studio/src/main/java/io/github/math0898/studio/ItemPreview.java
+++ studio/src/main/java/io/github/math0898/studio/ItemPreview.java
@@ -13,6 +13,36 @@ import java.awt.*;
 public class ItemPreview extends BasicGameObject {
 
     /**
+     * Base color of the background box.
+     */
+    private static final Color BACKGROUND_COLOR = new Color(17, 2, 17);
+
+    /**
+     * Border highlight color of the background box.
+     */
+    private static final Color HIGHLIGHT_COLOR = new Color(44, 8, 98);
+
+    /**
+     * The width of the background box.
+     */
+    private static final int BACKGROUND_BOX_WIDTH = 408;
+
+    /**
+     * The base height of the background box. TODO: This is scaled by the number of rows.
+     */
+    private static final int BACKGROUND_BOX_HEIGHT = 376;
+
+    /**
+     * The border between the background box and the inner box.
+     */
+    private static final int INNER_BORDER = 2;
+
+    /**
+     * The width of the inner border box.
+     */
+    private static final int INNER_BORDER_WIDTH = 2;
+
+    /**
      * Called every drawing frame so programs have a chance to make their voices heard on what gets drawn.
      *
      * @param width  The width of the pixel map.
@@ -21,6 +51,10 @@ public class ItemPreview extends BasicGameObject {
      */
     @Override
     public void applyChanges (int width, int height, GraphicsPanel panel) {
-        panel.setRectangle(100, 100, 50, 50, Color.WHITE);
+        final int posX = 100;
+        final int posY = 100;
+        panel.setRectangle(posX, posY, BACKGROUND_BOX_WIDTH, BACKGROUND_BOX_HEIGHT, BACKGROUND_COLOR);
+        panel.setRectangle(posX + INNER_BORDER, posY + INNER_BORDER, BACKGROUND_BOX_WIDTH - (2 * INNER_BORDER), BACKGROUND_BOX_HEIGHT - (2 * INNER_BORDER), HIGHLIGHT_COLOR);
+        panel.setRectangle(posX + INNER_BORDER + INNER_BORDER_WIDTH, posY + INNER_BORDER + INNER_BORDER_WIDTH, BACKGROUND_BOX_WIDTH - (2 * (INNER_BORDER_WIDTH + INNER_BORDER)), BACKGROUND_BOX_HEIGHT - (2 * (INNER_BORDER_WIDTH + INNER_BORDER)), BACKGROUND_COLOR);
     }
 }
